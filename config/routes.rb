423d1566d26a32208510f548a08daab0ade14376
Augustin Lazarus <augustin.lazarus@free.fr>
2020-05-12 16:10:24 +0200

    2nd challenge done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update', as: :update_task
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
